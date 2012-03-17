bbf0a54fe7792c183238cc06815034c5bde2c481
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-16 21:43:53 -0700

    show the chef stacktrace if there's an error

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,2 +1,4 @@
 # Runs application cookbooks
 sudo "/usr/local/ey_resin/ruby/bin/chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef/dna.json -l debug"
+
+run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
