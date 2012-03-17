3bf5dc559e0bc52e03eb54ce6d03d0480afc5c28
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-16 23:09:29 -0700

    enable_package supports unmasking... or whatever it is

--- deploy/cookbooks/emerge/definitions/enable_package.rb
+++ deploy/cookbooks/emerge/definitions/enable_package.rb
@@ -2,10 +2,19 @@ define :enable_package, :version => nil do
   name = params[:name]
   version = params[:version]
   full_name = name + ("-#{version}" if version)
+  unmask = params[:unmask] || false
 
   update_file "local portage package.keywords" do
     path "/etc/portage/package.keywords/local"
     body "=#{full_name}"
     not_if "grep '=#{full_name}' /etc/portage/package.keywords/local"
+  end
+  
+  if unmask
+    update_file "local portage package.unmask" do
+      path "/etc/portage/package.unmask/local"
+      body "=#{full_name}"
+      not_if "grep '=#{full_name}' /etc/portage/package.keywords/local"
+    end
   end
 end
--- deploy/cookbooks/qt-webkit/recipes/install.rb
+++ deploy/cookbooks/qt-webkit/recipes/install.rb
@@ -3,11 +3,12 @@
 # Recipe:: install
 #
 
-packages_to_unmask = %w[qt-core qt-gui qt-phonon qt-script qt-webkit qt-xmlpatterns]
+packages_to_unmask = %w[qt-core qt-gui qt-phonon qt-script qt-webkit qt-xmlpatterns qt-qt3support]
 
 packages_to_unmask.each do |package|
   enable_package "x11-libs/#{package}" do
     version node[:qt_webkit_version]
+    unmask true
   end
 end
 
