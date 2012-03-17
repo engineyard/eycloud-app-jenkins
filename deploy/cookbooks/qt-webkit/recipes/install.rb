c202d3f1606b7ab6c7a77d856b9f0ee1aabf4be6
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-16 22:58:02 -0700

    trying the 4.7.3 packages mentioned by kirk in /engineyard/portage/engineyard/profiles/common/package.mask

--- deploy/cookbooks/qt-webkit/recipes/install.rb
+++ deploy/cookbooks/qt-webkit/recipes/install.rb
@@ -3,7 +3,7 @@
 # Recipe:: install
 #
 
-packages_to_unmask = %w[qt-webkit qt-gui qt-script qt-core qt-qt3support qt-sql]
+packages_to_unmask = %w[qt-core qt-gui qt-phonon qt-script qt-webkit qt-xmlpatterns]
 
 packages_to_unmask.each do |package|
   enable_package "x11-libs/#{package}" do
