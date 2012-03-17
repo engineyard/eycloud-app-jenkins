4a21bd5dbdc581bcb35c505d2af69780aa24cd9b
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-16 22:53:03 -0700

    unmask lots of subpackages

--- deploy/cookbooks/qt-webkit/recipes/install.rb
+++ deploy/cookbooks/qt-webkit/recipes/install.rb
@@ -3,8 +3,12 @@
 # Recipe:: install
 #
 
-enable_package 'x11-libs/qt-webkit' do
-  version node[:qt_webkit_version]
+packages_to_unmask = %w[qt-webkit qt-gui qt-script qt-core qt-qt3support qt-sql]
+
+packages_to_unmask.each do |package|
+  enable_package "x11-libs/#{package}" do
+    version node[:qt_webkit_version]
+  end
 end
 
 package 'x11-libs/qt-webkit' do
