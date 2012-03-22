39212c1d86b5e791f9afe5806dee418f7c4b32f2
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-22 16:21:20 -0700

    upgrade to chef 0.10

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,13 @@
+# Set up a chef 0.10 dna.json file (for stack-v1 + stack-v2)
+# TODO does this run on non-app-master/solo?
+custom_json = node.dup
+custom_json['run_list'] = 'recipe[main]'
+File.open("/etc/chef-custom/dna.json", 'w') do |f|
+  f.puts JSON.pretty_generate(custom_json)
+  f.chmod(0600)
+end
+
 # Runs application cookbooks
-sudo "/usr/local/ey_resin/ruby/bin/chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef/dna.json"
+run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
 
 run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
