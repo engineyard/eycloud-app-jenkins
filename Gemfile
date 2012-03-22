cad5f8957e56183c7fe0afaa6a3951e6721f8a16
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-22 16:28:32 -0700

    use common helper gem

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 gem "rack"
 gem "rake"
 gem "chef"
+
+gem "eycloud-helper-common"
--- deploy/cookbooks/main/attributes/recipe.rb
+++ deploy/cookbooks/main/attributes/recipe.rb
@@ -1,3 +1,5 @@
-recipes('main')
-owner_name(@attribute[:users].first[:username])
-owner_pass(@attribute[:users].first[:password])
+if Chef::VERSION == '0.6.0.2'
+  recipes('main')
+  owner_name(@attribute[:users].first[:username])
+  owner_pass(@attribute[:users].first[:password])
+end
