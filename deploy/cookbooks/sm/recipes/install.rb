243709ac07ad35601b6328de704301efe4281e6b
Dr Nic Williams <drnicwilliams@gmail.com>
2012-06-07 10:46:01 -0500

    using latest released SM instead of master

--- deploy/cookbooks/sm/recipes/install.rb
+++ deploy/cookbooks/sm/recipes/install.rb
@@ -4,14 +4,11 @@ execute "install-sm" do
   command <<-SHELL
   for entry in /tmp/sm-* ; do rm -rf ${entry} ; done
 
-  curl -L -s https://github.com/sm/sm/tarball/master -o sm-master.tar.gz && 
-  tar zxf sm-master.tar.gz && # unpack
-  mv sm-sm-* sm-master &&     # rename github-named folder to something known
-  cd sm-master && 
-  ./install # >/dev/null
-
-
-  # prefix_path=/eystack ./install >/dev/null
+  version=$(curl https://smf.sh/releases/latest.txt) &&
+  curl -O https://smf.sh/releases/sm-${version}.tar.bz2 &&
+  tar jxf sm-${version}.tar.bz2 &&
+  cd sm-${version} &&
+  ./install
   SHELL
   action :run
 end
